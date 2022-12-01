af30633b5fa3f1252fdb83f1144c33757ef64b1b
huvidragon <116305056+huvidragon@users.noreply.github.com>
2022-12-01 12:39:25 -0600

    Update spark.js

--- spark.js
+++ spark.js
@@ -143,7 +143,7 @@ success: function (response) {
 		/* If adding the layer to the map, be sure to give it a name! */
 		
 		lyrControl.addOverlay(newLayer, "Overpass Query");
-		alert("Your layer has been added to the map!");
+		alert("Your layer has been added to the map! with " + overpass.features.length +" features !");
 	}
 }
 });
